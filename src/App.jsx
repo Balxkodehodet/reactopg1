7d8bda0c7d6902134088c29f6c7565bacbb8ce6d
Balxkodehodet <balx042025@gmail.com>
2025-08-15 10:14:14 +0200

    fixet imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import ListMenu from "./components/ListMenu.jsx";
+import KortComponent from "./components/KortComponent.jsx";
+import { meny } from "./RestaurantData.jsx";
 
 function App() {
   return (
